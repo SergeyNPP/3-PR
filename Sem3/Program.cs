8a7197f4fb8501ec03a6ce47e69f9e9c832658dc
SBudanov <budanoff.2011@yandex.ru>
2023-10-18 21:03:34 +0300

    push?

---
+++ Sem3/Program.cs
@@ -0,0 +1,31 @@
+﻿int FindQuards(int x, int y)
+
+// {
+//     if (x>0 && y>0)
+//     System.Console.WriteLine("Мы в 1 четверти");
+
+//     if (x<0 && y>0)
+//     System.Console.WriteLine("Мы в 2 четверти");
+    
+//     if (x<0 && y<0)
+//     System.Console.WriteLine("Мы в 3 четверти");
+    
+//     if (x>0 && y<0)
+//     System.Console.WriteLine("Мы в 4 четверти");
+// }
+
+{
+    if (x > 0 && y > 0) return 1;
+    else if (x < 0 && y > 0) return 2;
+    else if (x < 0 && y < 0) return 3;
+    else if (x > 0 && y < 0) return 4;
+    System.Console.WriteLine("The point lies on line!");
+    return 0;
+}
+
+System.Console.Write("Input X; ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input Y; ");
+int y = Convert.ToInt32(Console.ReadLine());
+int result = FindQuards(x, y);
+System.Console.WriteLine($"The point located at {result} quart");
